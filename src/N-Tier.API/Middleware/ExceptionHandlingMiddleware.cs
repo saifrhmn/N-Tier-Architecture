e7ff03b0c5d1c4e0e34d5059dd8b00edbe73a10d
Saifur Rahman <tuhin.rhmn@gmail.com>
2021-09-11 19:26:16 +0600

    n-tier architechture

---
+++ src/N-Tier.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using N_Tier.Application.Exceptions;
+using N_Tier.Application.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace N_Tier.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex.Message);
+
+            var code = HttpStatusCode.InternalServerError;
+            var errors = new List<string>() { ex.Message };
+
+            code = ex switch
+            {
+                NotFoundException => HttpStatusCode.NotFound,
+                BadRequestException => HttpStatusCode.BadRequest,
+                UnprocessableRequestException => HttpStatusCode.UnprocessableEntity,
+                _ => code
+            };
+
+            var result = JsonConvert.SerializeObject(ApiResult<string>.Failure((int)code, errors));
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ tests/N-Tier.Application.UnitTests/Services/BaseServiceTestConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using N_Tier.Application.MappingProfiles;
+using N_Tier.DataAccess.Repositories;
+using N_Tier.Shared.Services;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+
+namespace N_Tier.Application.UnitTests.Services
+{
+    public class BaseServiceTestConfiguration
+    {
+        protected readonly IMapper _mapper;
+        protected readonly IConfiguration _configuration;
+        protected readonly ITodoListRepository _todoListRepository;
+        protected readonly ITodoItemRepository _todoItemRepository;
+        protected readonly IClaimService _claimService;
+
+        public BaseServiceTestConfiguration()
+        {
+            _mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new TodoItemProfile());
+                cfg.AddProfile(new TodoListProfile());
+                cfg.AddProfile(new UserProfile());
+            }).CreateMapper();
+
+            var configurationBody = new Dictionary<string, string>
+            {
+                {"JwtConfiguration:SecretKey", "Super secret token key"},
+            };
+
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configurationBody)
+                .Build();
+
+            _todoListRepository = Substitute.For<ITodoListRepository>();
+            _todoItemRepository = Substitute.For<ITodoItemRepository>();
+
+            _claimService = Substitute.For<IClaimService>();
+            _claimService.GetUserId().Returns(new Guid().ToString());
+        }
+    }
+}
